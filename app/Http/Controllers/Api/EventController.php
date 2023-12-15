1c1e9b443bbb19662ffc8c6033c3d867b3080e6c
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-15 16:10:19 +0800

    Refactor EventController to use EventResource and add pagination
    This commit refactors the EventController to use the EventResource for returning events in the index and show methods. It also adds pagination to the index method. Additionally, the store method is updated to create a new event with validated request data and a default user_id. The update method is modified to update the event with validated request data. Finally, the destroy method is updated to delete the event and return a JSON response.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
@@ -13,7 +14,10 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        // return EventResource::collection(Event::all());
+        return EventResource::collection(
+            Event::with('user', 'attendees')->paginate()
+        );
     }
 
     /**
@@ -21,7 +25,17 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+                'name'        => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time'  => 'required|date',
+                'end_time'    => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return new EventResource($event);
     }
 
     /**
@@ -29,22 +43,36 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
-    }
+        $event->update(
+            $request->validate([
+                'name'        => 'sometimes|string|max:255',
+                'description' => 'nullable|string',
+                'start_time'  => 'sometimes|date',
+                'end_time'    => 'sometimes|date|after:start_time'
+            ])
+        );
 
-    /**
+        return new EventResource($event);
+    }
+    /** 
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        // return response(status: 204);
+        return response()->json([
+            'message' => 'Event deleted successfully'
+        ], 200);
     }
 }
