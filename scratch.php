cc7a59791687ec04e23a1fdebdcad7b54d172923
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-18 16:47:29 +0800

    Add scratch for reference

--- scratch.php
+++ scratch.php
@@ -82,3 +82,40 @@ return response()->json([
 ], 200);
 }
 }
+
+
+<!-- ATTENDEE CONTROLLER BEFORE (CANLOADRELATIONSHIP) -->
+{
+
+/**
+* Display a listing of the resource.
+*/
+public function index(Event $event)
+{
+$attendees = $event->attendees()->latest()
+
+return AttendeeResource::collection(
+$attendees->paginate()
+);
+}
+
+/**
+* Store a newly created resource in storage.
+*/
+public function store(Request $request, Event $event)
+{
+$attendee = $event->attendees()->create([
+'user_id' => 1,
+]);
+return new AttendeeResource($attendee);
+}
+
+/**
+* Display the specified resource.
+*/
+public function show(Event $event, Attendee $attendee)
+{
+return new AttendeeResource($attendee);
+}
+
+/**
